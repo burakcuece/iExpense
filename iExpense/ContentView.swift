8a3f4824bf4cf61bbdeb3e01408619375f4efab7
Burak Cüce <b.cuece@web.de>
2022-07-28 17:28:05 +0200

    - edit ContentView

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,19 +7,19 @@
 
 import SwiftUI
 
-struct User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
 }
 
 struct ContentView: View {
-    @State private var user = User()
+    @StateObject private var user = User()
     
     var body: some View {
         VStack {
             Text("Your name is \(user.firstName) \(user.lastName)")
             
-            TextField("First name", text: $user.firstName)
+            TextField("B", text: $user.firstName)
             TextField("Last name", text: $user.lastName)
 
         }
